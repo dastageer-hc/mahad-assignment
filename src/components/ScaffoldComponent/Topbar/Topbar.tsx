2b7b1ff2b97b56afbec8142dc0ba81184ad9a226
Dastageer H <dastageerh@Dastageers-MacBook-Air.local>
2023-12-01 16:07:17 +0530

    feature: topbar buttons added

--- src/components/ScaffoldComponent/Topbar/Topbar.tsx
+++ src/components/ScaffoldComponent/Topbar/Topbar.tsx
@@ -1,9 +1,12 @@
+import { MdOutlineQuestionMark } from "react-icons/md";
+import { FaPlus } from "react-icons/fa6";
 import logoHero from '../../../assets/logo-hero.png';
 
 function Topbar(){
     return <>
             {/* Top bar */}
             <div className="topbar-container bg-white min-h-topbarHeight flex items-center">  
+                
                 <div className="logo-hero w-14 h-12 ml-5 flex items-start
                  justify-center">
                     <img src={logoHero} alt="Logo" />
@@ -14,7 +17,27 @@ function Topbar(){
                      className='border-2  w-4/12 rounded-full border-slate-200 outline-gray-300 h-10 p-3'
                      placeholder="Search Fleetio"/>
                 </div>
-                <div className="topbar-buttons-container">test</div>
+
+                <div className="topbar-buttons-container">
+                    
+                    <button className="rounded-full
+                     border-2  
+                      focus:outline-none focus:ring
+                       focus:border-blue-300 p-1 mr-3" 
+                       >
+                        <MdOutlineQuestionMark className="text-slate-400
+                         color-slate-300 w-6 h-6" />
+                    </button>
+
+                    <button className="rounded-full
+                     border-2  
+                      focus:outline-none focus:ring
+                       focus:border-blue-300 p-1 mr-4" 
+                       >
+                        <FaPlus className="text-slate-400
+                         color-slate-300 w-6 h-6" />
+                    </button>
+                 </div>
 
             </div>
 
@@ -23,3 +46,6 @@ function Topbar(){
 }
 
 export default Topbar
+
+
+
